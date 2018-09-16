9ffe24791db0b7719fdb8c55d0fb72e8447bb186
Rohan Ayush <rohanayush05@gmail.com>
2018-09-16 23:24:32 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+	return (
+       <div className='bg-light-green dib br3 ma2 grow'>
+          <img alt="rohan" src='https://robohash.org/cool?500x500' />
+          <div>
+             <h2>Rohan Ayush</h2>
+             <p>rohanayush05@gmail.com</p>
+          </div>
+       </div>
+		);
+}
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+ReactDOM.render(
+        <div>
+         <Card />
+         <Card />
+	<Card />
+   </div>
+	, document.getElementById('root'));
+registerServiceWorker();
